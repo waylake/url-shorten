8517fb78e3e8d2e33ce7d8eb2fb230837aeb8384
dy.hwang <hwangdoyeonyu@gmail.com>
2024-07-15 11:58:13 +0900

    edit db connection code for atlas

--- backend/src/utils/db.ts
+++ backend/src/utils/db.ts
@@ -3,25 +3,23 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// Set the strictQuery option to suppress the warning
-mongoose.set("strictQuery", false);
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(
-      process.env.MONGODB_URI as string,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        ssl: false,
-        tls: false,
-        tlsAllowInvalidCertificates: true,
-        serverSelectionTimeoutMS: 60000, // 타임아웃 시간 증가
-      } as mongoose.ConnectOptions,
-    );
-    console.log("Connected to MongoDB");
+    console.log("Attempting to connect to MongoDB Atlas...");
+    await mongoose.connect(MONGODB_URI, {});
+    console.log("Connected to MongoDB Atlas");
+
+    mongoose.connection.on("error", (err) => {
+      console.error("MongoDB connection error:", err);
+    });
+
+    mongoose.connection.on("disconnected", () => {
+      console.log("MongoDB disconnected");
+    });
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
+    console.error("Error connecting to MongoDB Atlas:", error);
     process.exit(1);
   }
 };
