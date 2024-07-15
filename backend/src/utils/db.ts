7bc80c7054c1ae411fcb366c2aab3f7058fa2862
dy.hwang <hwangdoyeonyu@gmail.com>
2024-07-15 12:01:41 +0900

    edit db connection code for atlas

--- backend/src/utils/db.ts
+++ backend/src/utils/db.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017";
+mongoose.set("strictQuery", false);
 
 export const connectDB = async () => {
   try {
